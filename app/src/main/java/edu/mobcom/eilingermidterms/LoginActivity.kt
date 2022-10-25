485abd146b47afe8e7aeef1f01b4a685222d8737
UBSITEILINGER <20137582@s.ubaguio.edu>
2022-10-25 14:46:15 +0800

    asdasdsadefsdfsd

--- app/src/main/java/edu/mobcom/eilingermidterms/LoginActivity.kt
+++ app/src/main/java/edu/mobcom/eilingermidterms/LoginActivity.kt
@@ -3,24 +3,42 @@ package edu.mobcom.eilingermidterms
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val LOGIN: Button = findViewById(R.id.Login)
+        LOGIN.setOnClickListener(this)
         val REG: Button = findViewById(R.id.Register)
 
-        LOGIN.setOnClickListener {
-            val i= Intent(this, MessageActivity::class.java)
-            startActivity(i)
-        }
 
         REG.setOnClickListener {
             val i= Intent(this, RegisterActivity::class.java)
             startActivity(i)
+        }
+    }
+    private fun openLogin() {
+        val i= Intent(this, MessageActivity::class.java)
+        startActivity(i)
+    }
+
+    private fun openReg() {
+        val i= Intent(this, RegisterActivity::class.java)
+        startActivity(i)
+    }
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.Register->{
+                openReg()
+            }
+
+            R.id.Login->{
+                openLogin()
+            }
         }
     }
 }
